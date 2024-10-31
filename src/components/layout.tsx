ac0d809c254ac96ac3788f42105e3fd2b0d7840c
Liuzhch1 <liuhiheng@outlook.com>
2024-10-31 20:24:30 +0800

    feat: self intro in home page

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -12,9 +12,8 @@ interface NavLink {
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const pathname = usePathname()
   const links: NavLink[] = [
-    { path: '/', text: 'Home' },
     { path: '/posts', text: 'Posts' },
-    { path: '/about', text: 'About Me' },
+    { path: '/about', text: 'About' },
   ]
 
   const isCurrentPage = (path: string): 'page' | undefined => {
@@ -22,27 +21,35 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   }
 
   return (
-    <div className="max-w-3xl mx-auto">
-      <header className="flex p-4 pb-3.5 border-b border-dashed border-gray-200">
+    <div className="max-w-[44rem] mx-auto">
+      <header
+        className={`flex justify-between p-4 pb-3.5 ${
+          pathname !== '/' ? 'pb-12' : 'pb-1'
+        }`}
+      >
         <div className="flex items-center gap-4 box-border">
-          <Link href="/" className="text-base font-bold no-underline mr-4">
-            Joey's Blog
+          <Link href="/" className="text-2xl font-bold no-underline">
+            Joey Liu
+            <span className="text-sm text-gray-400 pl-3 font-normal">
+              aka liuzhch1
+            </span>
           </Link>
-          <nav>
-            {links.map((link) => (
-              <Link
-                key={link.path}
-                href={link.path}
-                className={`ml-4 leading-normal hover:underline ${
-                  isCurrentPage(link.path) ? 'underline' : ''
-                }`}
-                aria-current={isCurrentPage(link.path)}
-              >
-                {link.text}
-              </Link>
-            ))}
-          </nav>
         </div>
+        <nav className="flex">
+          {links.map((link) => (
+            <Link
+              key={link.path}
+              href={link.path}
+              className={`text-lg font-medium ml-4 mr-4 leading-normal hover:underline ${
+                isCurrentPage(link.path)
+                  ? 'underline text-gray-900 dark:text-gray-200'
+                  : 'no-underline text-gray-500 dark:text-gray-400'
+              }`}
+            >
+              {link.text}
+            </Link>
+          ))}
+        </nav>
       </header>
       <main className="p-4 [&>:first-child]:mt-0">{children}</main>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,77 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Joey's Blog</h1>
-      <p>This is the home page of my personal blog.</p>
-    </div>
+    <>
+      <Head>
+        <title>Joey Liu's Page</title>
+        <meta name="description" content="Joey Liu's personal page" />
+      </Head>
+      <div>
+        <p className="text-lg m-0">
+          <span className="text-gray-400">I'm a </span>software engineer,
+          <br />
+          <span className="text-gray-400">more of a </span>
+          gamer<span className="text-gray-400">, and</span>
+          <br />
+          fall savorer<span className="text-gray-400">.</span>
+          <br />{' '}
+        </p>
+        <p className="text-lg m-0 mt-12">
+          I currently working at{' '}
+          <a href="https://www.hiorka.com/" target="_blank">
+            Orka
+          </a>
+          , aiming to make hearing aids more accessible.
+        </p>
+        <p className="text-lg m-0 mt-6">
+          I'm passionate about exploring and crafting elegant, creative
+          softwares. I love sharing them with others and exchanging experiences,
+          to bring efficiency, inspiration, and simple joy to those around me.
+          <br />
+          Recently, I'm recommending{' '}
+          <a href="https://www.cursor.com" target="_blank">
+            Cursor
+          </a>{' '}
+          for code composing, and{' '}
+          <a href="https://claude.ai/new" target="_blank">
+            Claude
+          </a>{' '}
+          for sparking creativity.
+        </p>
+        <p className="text-lg m-0 mt-6">
+          Besides, I'm an avid single-player gamer who enjoys challenging action
+          games like{' '}
+          <a href="https://store.steampowered.com/app/814380" target="_blank">
+            Sekiro: Shadows Die Twice
+          </a>{' '}
+          and{' '}
+          <a href="https://store.steampowered.com/app/2138710" target="_blank">
+            Sifu
+          </a>
+          , along with indie gems such as{' '}
+          <a href="https://store.steampowered.com/app/813230" target="_blank">
+            Animal Well
+          </a>
+          ,{' '}
+          <a href="https://store.steampowered.com/app/460950" target="_blank">
+            Katana Zero
+          </a>
+          , and{' '}
+          <a href="https://store.steampowered.com/app/504230" target="_blank">
+            Celeste
+          </a>
+          . Lately, I've been completely hooked on{' '}
+          <a href="https://store.steampowered.com/app/418240" target="_blank">
+            Shadow Tactics
+          </a>
+          .
+        </p>
+        <p className="text-lg m-0 mt-6">
+          Fall is my favorite season, enveloping both mind and body in its
+          peaceful, cozy embrace.
+        </p>
+      </div>
+    </>
   )
 }
