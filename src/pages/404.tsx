0029d00535e1af0260ff6ac65ad1a7d64b25ca75
Liuzhch1 <liuhiheng@outlook.com>
2024-11-01 21:16:58 +0800

    feat: align 404 left to unify style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,10 +8,10 @@ export default function Custom404() {
         <title>404 - Page Not Found</title>
         <meta name="description" content="Page not found" />
       </Head>
-      <div className="flex flex-col items-center justify-center min-h-[40vh]">
+      <div>
         <h2 className=" font-bold mb-4">404 - Page Not Found</h2>
         <p className="text-lg">
-          It's ok, let's <Link href="/">go back home</Link>
+          No worries, let's <Link href="/">go back home</Link>.
         </p>
       </div>
     </>
