b4dcd36511eef47494bee7fb2a1ff16b6054c2bc
Liuzhch1 <liuhiheng@outlook.com>
2024-11-01 12:49:18 +0800

    feat: about page content

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,3 +1,32 @@
+import Head from 'next/head'
+
 export default function About() {
-  return <div>About</div>
+  return (
+    <>
+      <Head>
+        <title>About - Joey's Blog</title>
+        <meta name="description" content="About Joey" />
+      </Head>
+      <div className="text-lg">
+        <p>
+          This personal website was built in a fall raining day, though fall is
+          my favorite season, I don't like its rainy parts.
+        </p>
+        <p>Design inspiration drawn from:</p>
+        <ul>
+          <li>
+            <a href="https://www.ekzhang.com/">Eric Zhang</a>
+          </li>
+          <li>
+            <a href="https://sgt.hootr.club/">Molten Matter</a>
+          </li>
+          <li>
+            <a href="https://github.com/11ty/eleventy-base-blog">
+              Eleventy Base Blog
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  )
 }
