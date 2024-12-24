4f5b322a46d1bca933dc4b66c61c1bbc3e0e71e1
Liuzhch1 <liuhiheng@outlook.com>
2024-12-24 09:35:21 +0800

    fix: utc...

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -10,5 +10,8 @@ export const formatDate = ({
 }): string => {
   const parsedDate = typeof date === 'string' ? parseISO(date) : date
   const tzDate = new TZDate(parsedDate, 'Asia/Shanghai')
+  if (process.env.NODE_ENV !== 'development') {
+    tzDate.setHours(tzDate.getHours() - 8)
+  }
   return format(tzDate, time ? 'yyyy/MM/dd HH:mm' : 'yyyy/MM/dd')
 }
